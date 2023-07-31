81bb8a0180b55c6c560c42d9aa7f4a7dcea85894
Manuel Digregorio <digregorio.mr@gmail.com>
2023-07-31 17:41:14 -0300

    image banner general styling

--- src/app/components/ImageBanner.tsx
+++ src/app/components/ImageBanner.tsx
@@ -5,20 +5,20 @@ import Image from 'next/image';
 
 export default function ImageBanner() {
   return (
-    <div className="bg-violet-500">
+    <div className="bg-gray-100">
       <div className="mx-auto max-w-xl flex flex-col justify-center items-center gap-4 px-2 py-2 sm:px-4 sm:py-6 lg:max-w-7xl lg:flex-row lg:px-8">
-        <div>
-          <h2 className="text-3xl font-bold tracking-tight sm:text-7xl text-gray-800">
+        <div className="flex flex-col justify-center">
+          <h2 className="text-3xl font-bold tracking-tight sm:text-7xl text-gray-700">
             Welcome to Tech Zone!
           </h2>
-          <p className="text-justify mr-16 mt-4 sm:text-xl text-gray-800">
+          <p className="text-justify mr-16 mt-4 sm:text-xl text-gray-400">
           We're your go-to home webstore for the latest and greatest tech products. 
           Discover innovation at its finest with our carefully curated selection of cutting-edge gadgets and accessories. 
           From smartphones to smart home devices, we've got what you need to elevate your digital lifestyle. 
           Shop now and experience the future with TechZone!
           </p>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center items-center">
           <Image
             src="/banner.jpeg"
             width={1200}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
         <ResponsiveAppBar/> 
         </div>
 
-        <div className="flex flex-col items-center justify-center sm:mx-0 md:mx-8 lg:mx-12 ">
-          <div className="lg:max-w-7xl sm:max-w-sm md:max-w-md mx-auto px-6 py-12 bg-white rounded-lg shadow-lg">
+        <div className="flex flex-col items-center justify-center bg-gray-200 sm:mx-0 md:mx-8 lg:mx-12 ">
+          <div className="lg:max-w-7xl sm:max-w-sm md:max-w-md mx-auto px-6 py-12 bg-gray-100 rounded-lg shadow-lg">
         {children}
           </div>
         </div>
