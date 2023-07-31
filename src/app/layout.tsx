1594bd8d1766f95552d6da74fce26fc15086737e
Manuel Digregorio <digregorio.mr@gmail.com>
2023-07-31 17:17:42 -0300

    fixed nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,9 @@ export default function RootLayout({ children }) {
       </head>
       <body>
 
+        <div className="fixed top-0 left-0 w-full bg-white shadow-lg">
         <ResponsiveAppBar/> 
+        </div>
 
         <div className="flex flex-col items-center justify-center sm:mx-0 md:mx-8 lg:mx-12 ">
           <div className="lg:max-w-7xl sm:max-w-sm md:max-w-md mx-auto px-6 py-12 bg-white rounded-lg shadow-lg">
