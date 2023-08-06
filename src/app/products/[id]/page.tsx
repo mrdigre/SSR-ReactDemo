d217f47060ad28494b87844042cc917c170c812f
Manuel Digregorio <digregorio.mr@gmail.com>
2023-08-06 10:23:43 -0300

    product page first structure

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,47 @@
+import { getProduct } from "@/lib/prisma";
+import Image from "next/image";
 
-export default function Page({ params }) {
-    return <div className="bg-black">My Post: {params.id}</div>
-  }
+const Page = async ({ params }) => {
+  const products = await getProduct( params.id );
+  
+  return(
+    <div className="bg-black mt-6 flex flex-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      
+      <div>
+        <div>
+          {products.name}
+        </div>
+        <div>
+          {products.model}
+        </div>
+        <div>
+          {products.description}
+        </div>
+        <div>
+          {products.price}
+        </div>
+        <div className="flex flex-cols-2">
+          <div className="p-4">
+            <button>ADD TO CART</button>
+          </div>
+          <div className="p-4">
+            QUANTITY
+          </div>
+        </div>
+      </div>
+      
+      <div>
+        <Image
+          src={products.image}
+          alt={products.name}
+          className="object-center"
+          width={'100'}
+          height={'100'}
+        />
+      </div>
+
+    </div>
+  );
+};
+export default Page;
+
