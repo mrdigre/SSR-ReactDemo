a040af14416c8a6b61affbb50707f7a813575777
Manuel Digregorio <digregorio.mr@gmail.com>
2023-08-07 13:11:54 -0300

    product page styling

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,17 +3,31 @@ import Image from "next/image";
 
 const Page = async ({ params }) => {
   const products = await getProduct( params.id );
-  
+
   return(
-    <div className="text-black bg-gray-200 w-full items-center relative mt-6 flex gap-x-6 gap-y-10 sm:flex-cols-1 lg:flex-cols-2 xl:gap-x-8">
+    <div className="text-black bg-gray-100 flex w-full border rounded-lg items-center justify-evenly relative mt-6 sm:flex-cols-1 lg:flex-cols-2 xl:gap-x-8">
       
-      <div className="w-full sm:w-1/2 bg-white">
-        <h1 className="text-3xl font-semibold mb-2 text-center"> {products.name} </h1>
-        <h2> {products.model} </h2>
-        <h3> {products.description} </h3>
+      <div className="w-1/3 bg-white justify-center border border-2 rounded-lg">
+        
+        <div className="text-black bg-gray-100 flex w-full items-center">
+          <h1 className="text-3xl font-semibold mb-2 text-center">
+            {products.name} 
+          </h1>
+        </div>  
+
+      <div>
+        <h2> 
+          {products.model} 
+        </h2>
+        <h3>
+          {products.description} 
+        </h3>
         <div className="text-xl font-bold text-right">
           ${products.price}
         </div>
+      </div>
+
+      <div>
         <div className="flex flex-cols-2">
           <div className="p-4">
             <button>ADD TO CART</button>
@@ -23,21 +37,20 @@ const Page = async ({ params }) => {
           </div>
         </div>
       </div>
-      
-      <div className="w-full flex justify-center">
-        <div className="w-full sm:w-1/2">
+</div>
+      <div className="w-1/3 min-h-[50%] bg-white flex justify-center items-center border border-2 rounded-lg">
+        <div className="p-6">
           <Image
             src={products.image}
             alt={products.name}
             className="object-center"
-            width={'300'}
-            height={'300'}
+            width={'500'}
+            height={'500'}
           />
         </div>
       </div>
-    
-    </div>
+
+      </div>
   );
 };
 export default Page;
-
