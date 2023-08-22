cc14615ec57918a310ff469baed1beb1eb42b6b0
Manuel Digregorio <digregorio.mr@gmail.com>
2023-08-22 09:36:30 -0300

    changed repeated shopping cart icon for user icon

--- src/app/components/ResponsiveAppBar.tsx
+++ src/app/components/ResponsiveAppBar.tsx
@@ -93,7 +93,7 @@ export default function ResponsiveAppBar() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-8">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="rounded-full bg-gray-800 p-3  text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
                       <UserIcon className="h-8 w-8" aria-hidden="true" />
                     </Menu.Button>
