964b26107d40c2f3659482cae04d7349e95dfab2
Manuel Digregorio <digregorio.mr@gmail.com>
2023-08-30 12:53:56 -0300

    product page layout and styling

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -9,8 +9,6 @@ const ProductPage = async ({ params, product }) => {
   
    return(
     
-
-   
         <div className="mt-4 bg-gray-200 w-full">
             <div className="flex flex-col h-2/3 md:flex-row -mx-4 mt-6 p-6">
                 <div className="flex justify-center items-center md:flex-1 bg-white shadow-lg rounded-lg">
@@ -19,46 +17,55 @@ const ProductPage = async ({ params, product }) => {
                         <Image
                             src={fetchedProduct.image}
                             alt={fetchedProduct.name}
-                            width={"300"}
-                            height={"300"}
+                            width={"400"}
+                            height={"400"}
                         />
                     </div>
                     
                 </div>
                 
-                <div className="md:flex-1 px-4 mt-4">
-                    <h2 className="text-2xl text-gray-600 font-bold mb-2">{fetchedProduct?.name}</h2>
-                    <p className="text-gray-600 text-sm mb-4"> {fetchedProduct?.description} </p>
-                        <div className="flex mt-4">
-                            <div className="mr-4">
-                                <span className="font-bold text-gray-700">Price:</span>
-                                <span className="text-gray-600"> ${fetchedProduct?.price} </span>
-                            </div>
-                            <div>
-                                <span className="font-bold text-gray-700">Stock:</span>
-                                <span className="text-gray-600"> {fetchedProduct?.stock} </span>
-                            </div>
+                <div className="md:flex-1 p-4 ml-4">
+                    
+                    <h2 className="text-4xl text-gray-600 font-bold mb-4">{fetchedProduct?.name}</h2>
+                    <p className="text-2xl text-gray-600 mb-4"> {fetchedProduct?.title} </p>
+                        
+                    <div className="flex flex-row mt-4 w-full mb-4">
+                        <div className="flex justify-left w-1/4">
+                            <span className="font-bold text-gray-700">Price: </span>
+                            <span className="text-gray-600"> ${fetchedProduct?.price} </span>
                         </div>
-                
-                
-                
-                        <div>
-                            <span className="font-bold text-gray-700 mt-2">Product Description:</span>
-                            <p className="text-gray-600 text-sm mt-2"> {fetchedProduct?.description} </p>
+                        <div className="flex w-1/4 justify-end">
+                            <span className="font-bold text-gray-700">Stock: </span>
+                            <span className="text-gray-600"> {fetchedProduct?.stock} </span>
                         </div>
-                <div className="flex -mx-2 mb-4 mt-4">
-                        <div className="w-1/2">
+                    </div>
+                
+                
+                
+                    <div className="mb-4">
+                        <span className="font-bold text-gray-700 mt-2 text-bg">Product Description:</span>
+                        <p className="text-gray-600 mt-2"> {fetchedProduct?.description} </p>
+                    </div>
+                        
+                    <div className="flex flex-col mb-4 text-gray-600 font bold w-1/2 text-sm">
+                            Select Quantity
+                            <input type="number" className="border rounded-lg py-1 w-1/4">
+                            </input>
+                    </div>
+
+                    <div className="flex w-full -mx-1 mb-4 mt-4">
+                        <div className="flex w-1/2">
                             <CartButton product={fetchedProduct} />
                         </div>
-                        {/*   ADD TO WISH LIST BTN?   */}
-                        {/* <div className="w-1/2 px-2">
-                            <button className="w-full bg-gray-400 text-gray-800 py-2 px-4 rounded-full font-bold hover:bg-gray-300">Add to Wishlist</button>
-                        </div> */}
+                            {/*   ADD TO WISH LIST BTN?   */}
+                            {/* <div className="w-1/2 px-2">
+                                <button className="w-full bg-gray-400 text-gray-800 py-2 px-4 rounded-full font-bold hover:bg-gray-300">Add to Wishlist</button>
+                            </div> */}
+                    </div>
+
+                    
+                
                 </div>
-            </div>
-
-
-                                
             </div>
         </div>
    
