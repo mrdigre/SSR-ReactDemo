44864e331ac8b56aef7028d38c403df80cc390d2
Milton Tulli <milton.tulli@gmail.com>
2023-09-14 14:58:14 -0300

    ommit gitadd from lint-staged file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx,json}': ['eslint  --cache --cache-location .next/cache/eslint/', 'npm run format', "git add"]
+  'src/**/*.{js,jsx,ts,tsx,json}': ['eslint  --cache --cache-location .next/cache/eslint/', 'npm run format']
 };
 
