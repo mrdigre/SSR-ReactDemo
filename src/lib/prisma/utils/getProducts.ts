2f5c06b460508158b9e2a86afeeb0846586d1087
Manuel Digregorio <manueldigregorio@Manuels-Air.lan>
2023-09-19 19:01:32 -0300

    lint errors fixed

--- src/lib/prisma/utils/getProducts.ts
+++ src/lib/prisma/utils/getProducts.ts
@@ -1,5 +1,5 @@
 import prisma from '../prisma';
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function getProducts(args: any = {}) {
   return prisma.product.findMany(args);
 }
