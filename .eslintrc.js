a11cee78b79e8b14341bc147569566a6682ee4fd
Milton Tulli <milton.tulli@gmail.com>
2023-09-19 13:43:55 -0300

    improve typescript eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "extends": ["next", "prettier"],
-  "rules": {
+  extends: ['plugin:@typescript-eslint/recommended', "next", "prettier"],
+  rules: {
     // "no-param-reassign": 0,
     // "no-console": 1,
     // "no-const-assign": 2,
     // "no-this-before-super": 2,
     // "no-undef": 1,
     // "no-unreachable": 2,
-    // "no-unused-vars": 2,
     "@next/next/no-html-link-for-pages": "off"
   },
-  "parserOptions": {
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
     "babelOptions": {
       "presets": [require.resolve("next/babel")],
     },
