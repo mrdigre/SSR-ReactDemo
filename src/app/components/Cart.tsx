63c59d0ec42abd6fc9192bfb667901e6c242532e
Manuel Digregorio <manueldigregorio@Manuels-Air.lan>
2023-09-24 02:23:25 -0300

    cart styling

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -9,7 +9,7 @@ const Cart = () => {
   const showMessageIfEmptyCart = totalQuantity === 0;
 
   return (
-    <div className="flex flex-col bg-black w-full text-black mt-6 p-2">
+    <div className="flex flex-col bg-white w-full text-black mt-6 p-2">
       <header className="bg-white rounded-md border-gray-500">
         <h1 className="text-3xl font-bold text-left py-4">
           {totalQuantity} products currently in Cart!
@@ -30,56 +30,59 @@ const Cart = () => {
         )}
       </header>
 
-      <main className="flex flex-col w-full px-4 py-8 md:px-8 bg-white mt-4">
+      <main className="flex flex-col w-full px-4 py-2 md:px-8 bg-white mt-4">
         <section>
-          <ul className="space-y-4">
+          <ul className="space-y-2">
             {cartItems.map((item, index) => (
-              <li className="bg-white shadow-md p-4" key={index}>
+              <li className="bg-white shadow-md p-4 rounded-md" key={index}>
                 <div className="flex flex-row w-full ">
-                  <div className="flex flex-col">
-                    <h3 className="text-lg font-semibold">{item.name}</h3>
+                  <div className="flex flex-col items-center justify-center w-1/4">
                     <Image
                       src={item.image}
                       alt={item.name}
-                      width={200}
-                      height={100}
+                      width={100}
+                      height={50}
                       className="rounded-lg bg-gray-100 mt-4"
                     />
                   </div>
 
-                  <div className="flex items-center justify-right">
-                    {/* <p className="text-gray-600 mb-2">Price: ${item.price.toFixed(2)}</p> */}
+                  <div className="flex flex-col justify-left w-1/3">
+                    <h3 className="text-lg font-semibold">{item.name}</h3>
+                    <p className="text-gray-600 mb-2">{item.description}</p>
+                    <p className="text-gray-600 mb-2 font-bold">
+                      Price: ${item.price.toFixed(2)}
+                    </p>
 
-                    <button
-                      onClick={() => {
-                        if (item.quantity > 1) {
-                          modifyProductQuantity(item.id, item.quantity - 1);
-                        } else {
-                          removeFromCart(item.id);
+                    <div className="flex flex-row text-gray-600">
+                      <p className="pr-2">Quantity:</p>
+                      <button
+                        className="px-2 font-bold bg-gray-400 border-gray-200 rounded-md hover:bg-sky-300 text-white items-center"
+                        onClick={() => {
+                          if (item.quantity > 1) {
+                            modifyProductQuantity(item.id, item.quantity - 1);
+                          } else {
+                            removeFromCart(item.id);
+                          }
+                        }}
+                      >
+                        -
+                      </button>
+                      <p className="font-bold px-2">{item.quantity}</p>
+                      <button
+                        className="px-2 font-bold bg-gray-400 border-gray-200 rounded-md hover:bg-sky-300 text-white items-center"
+                        onClick={() =>
+                          modifyProductQuantity(item.id, item.quantity + 1)
                         }
-                      }}
-                    >
-                      -
-                    </button>
-
-                    <p className="text-gray-600">Quantity: {item.quantity}</p>
-
-                    <button
-                      onClick={() =>
-                        modifyProductQuantity(item.id, item.quantity + 1)
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
-                  {/* remove button */}
-                  <div>
-                    <button
-                      onClick={() => removeFromCart(item.id)}
-                      className="text-red-500 mt-2"
-                    >
-                      Remove
-                    </button>
+                      >
+                        +
+                      </button>
+                      <button
+                        onClick={() => removeFromCart(item.id)}
+                        className="text-red-500 ml-4"
+                      >
+                        Remove
+                      </button>
+                    </div>
                   </div>
                 </div>
               </li>
@@ -87,6 +90,8 @@ const Cart = () => {
           </ul>
         </section>
       </main>
+
+      <div className="flex w-3/4 justify-end text-bold">Subtotal:</div>
     </div>
   );
 };
